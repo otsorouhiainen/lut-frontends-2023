48a3c7b8644d4dd776ff7cbbb259d9edcf8671cc
Otso <otso.rouhiainen@gmail.com>
2023-07-12 16:59:00 +0300

    adjust to different screen sizes + mobile menu

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,8 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');    
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
 
 // animation for the movie/series ranking pages
 document.addEventListener("DOMContentLoaded", function() {
@@ -39,3 +44,38 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 200 * index);
   });
 });
+
+//Set initial state of menu
+
+// Set initial state as visible
+let showMenu = true;
+
+// Apply initial CSS classes for a visible menu
+menuBtn.classList.add('close');
+menu.classList.add('show');
+menuNav.classList.add('show');
+navItems.forEach(item => item.classList.add('show'));
+
+// Add event listener
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  // Check if the screen size is smaller than the medium breakpoint
+  if (window.matchMedia('(max-width: 768px)').matches) {
+    // Toggle the menu visibility
+    if (showMenu) {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      menuNav.classList.remove('show');
+      navItems.forEach(item => item.classList.remove('show'));
+    } else {
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      navItems.forEach(item => item.classList.add('show'));
+    }
+
+    // Update menu state
+    showMenu = !showMenu;
+  }
+}
